4a6b604295d08a1d6429c270cf502dc4c10f08d9
tboonsawas <tboonsawas@berkeley.edu>
2019-07-25 02:10:26 -0400

    added user pages for p3

--- draw/urls.py
+++ draw/urls.py
@@ -1,10 +1,12 @@
 # chat/urls.py
+from django.urls import path
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    path('user1/', views.user1, name='user1'),
+    path('user2/', views.user2, name='user2'),
+    path('user3/', views.user3, name='user3'),
 ]
 
--- draw/views.py
+++ draw/views.py
@@ -5,7 +5,11 @@ import json
 def index(request):
     return render(request, 'draw/index.html', {})
 
-def room(request, room_name):
-    return render(request, 'draw/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
-    })
+def user1(request):
+    return render(request, 'draw/user1.html', {})
+
+def user2(request):
+    return render(request, 'draw/user2.html', {})
+
+def user3(request):
+    return render(request, 'draw/user3.html', {})
