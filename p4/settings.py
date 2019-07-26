5db398d4b1594d1dac7c080f9c27f6c5c1377d0e
tboonsawas <tboonsawas@berkeley.edu>
2019-07-26 00:21:15 -0400

    Sockets work

--- p4/settings.py
+++ p4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*6t50m_p^gif@4sz!590)u_!#9c-u8l9+7-v+*_op=v^#7wkv9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["p3-websockets-tffnybnsws-tboonsawas508393.codeanyapp.com"]
+ALLOWED_HOSTS = ["p3-collab-pet-caretaking-app-tboonsawas508393.codeanyapp.com"]
 
 
 # Application definition
